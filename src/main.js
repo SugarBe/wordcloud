a149f2721afe62632045e956577bbc6059c4e75e
unknown <1094252227@qq.com>
2020-06-02 21:41:13 +0800

    初始化词云组件

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+import echarts from 'echarts'
 
 Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts
 
 new Vue({
   router,
